d002de48cb184e24ee23a0c8a1702eccaa8684ef
ilia YG_Home <yegorov86@gmail.com>
2021-02-19 17:46:47 +0200

    'web7.1'

--- web7(2)/Form1.cs
+++ web7(2)/Form1.cs
@@ -5,43 +5,9 @@ namespace web7_2_
 {
 	public partial class Form1 : Form
 	{
-		int answer;
-		int count = 0;
-		int dialogResult;
 		public Form1 ()
 		{
 			InitializeComponent();
-			Random rnd = new Random();
-			answer = rnd.Next(1, 100);
-		}
-
-		private void button1_Click (object sender, EventArgs e)
-		{
-			count++;
-			if (count < 6)
-			{
-				label2.Text = textBox1.Text;
-				if (Int32.TryParse(label2.Text, out dialogResult))
-					Compare(answer, dialogResult);
-			}
-			else
-				MessageBox.Show($"Вы проиграли! Верный ответ = {answer}",
-"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-			textBox1.Clear();
-		}
-
-		private void Compare (int answer, int dialogResult)
-		{
-			if (answer < dialogResult)
-				MessageBox.Show($"Не верно! Нужно меньшее число!",
-				"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-			if (answer > dialogResult)
-				MessageBox.Show($"Не верно! Нужно большее число!",
-					"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-			if (answer == dialogResult)
-				MessageBox.Show($"Поздравляю! Верный ответ = {answer}",
-	"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-			label2.Text = "0";
 		}
 
 		private void toolStripMenuItem1_Click (object sender, EventArgs e)
@@ -49,6 +15,7 @@ namespace web7_2_
 			Form2 newMDIChild = new Form2();
 			newMDIChild.MdiParent = this;
 			newMDIChild.Show();
+			button2.Hide();
 		}
 	}
 }
--- web7(2)/Form2.cs
+++ web7(2)/Form2.cs
@@ -1,12 +1,52 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace web7_2_
 {
 	public partial class Form2 : Form
 	{
+		public int answer;
+		public int count = 0;
+		public int dialogResult;
 		public Form2 ()
 		{
 			InitializeComponent();
+			Random rnd = new Random();
+			answer = rnd.Next(1, 100);
+		}
+		private void button1_Click (object sender, EventArgs e)
+		{
+			count++;
+			if (count < 6)
+			{
+				label2.Text = textBox1.Text;
+				if (Int32.TryParse(label2.Text, out dialogResult))
+					Compare(answer, dialogResult);
+			}
+			else
+				MessageBox.Show($"Вы проиграли! Верный ответ = {answer}",
+"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			textBox1.Clear();
+		}
+
+		private void Compare (int answer, int dialogResult)
+		{
+			if (answer < dialogResult)
+				MessageBox.Show($"Не верно! Нужно меньшее число!",
+				"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			if (answer > dialogResult)
+				MessageBox.Show($"Не верно! Нужно большее число!",
+					"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			if (answer == dialogResult)
+				MessageBox.Show($"Поздравляю! Верный ответ = {answer}",
+	"Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			label2.Text = "0";
+		}
+
+		private void textBox1_TextChanged (object sender, EventArgs e)
+		{
+
 		}
 	}
+
 }
