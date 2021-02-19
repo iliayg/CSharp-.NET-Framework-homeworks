3fe5d2744b64de04d3cf8e4b9037385d184127f1
ilia YG_Home <yegorov86@gmail.com>
2021-02-19 19:15:18 +0200

    'web7.1'

---
+++ web1(5)/Program.cs
@@ -0,0 +1,29 @@
+﻿//Давайте поупражняемся с работой в консоли. Напишем для этого несколько функций, которые будут выводить данные в определённом месте консоли. А также создадим перегрузку метода, в котором можно будет задать цвет символов:
+
+using System;
+
+namespace web1_5_
+{
+    class Program
+    {
+        static void Main ()
+        {
+            string surnameInput = Console.ReadLine();
+            string nameInput = Console.ReadLine();
+            string cityInput = Console.ReadLine();
+            Console.Clear();
+            OutputHelpers.PrintInCenter($"{ surnameInput} { nameInput}, г. { cityInput}");
+            Console.ReadKey();
+        }
+        public class OutputHelpers
+        {
+            public static void PrintInCenter (string text)
+            {
+                Console.SetCursorPosition(
+                    (Console.WindowWidth - text.Length) / 2, 
+                    Console.WindowHeight / 2 - 1);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ web2(7)/Program.cs
@@ -0,0 +1,40 @@
+﻿// a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+// б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+//================================================================================
+
+using System;
+namespace web2_7_
+{
+    class Program
+    {
+        static void PrintNumber (int a, int b)
+        {
+            if (a <= b)
+            {
+                Console.WriteLine(a);
+                a++;
+                PrintNumber(a, b);
+            }
+        }
+        static int SumNumber (int a, int b)
+        {
+            if (a <= b)
+            {
+                var sum = SumNumber(a + 1, b);
+                return sum + a;
+            }
+            return 0;
+        }
+        static void Main ()
+        {
+            Console.Write("Введите число a: ");
+            var d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            var e = Convert.ToInt32(Console.ReadLine());
+
+            PrintNumber(d, e);
+            Console.Write($"\nСумма всех чисел равна: { SumNumber(d, e)}");            
+            Console.ReadKey();
+        }
+    }
+}
